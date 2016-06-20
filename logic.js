c415f49ced9efc557d189fd84b947e31d081c3f9
patnir <rahul20patni@gmail.com>
2016-06-20 13:53:38 -0400

    Add transitions for both pages and text boxes

--- logic.js
+++ logic.js
@@ -1,1 +1,17 @@
-﻿
+﻿/// <reference path="index.html">
+
+function body_load() {
+    btnNavAddContact.onmousedown = btnNavAddContact_onmousedown;
+    btnNavShowContacts.onmousedown = btnNavShowContacts_onmousedown;
+}
+
+
+function btnNavAddContact_onmousedown() {
+    showInformation.style.marginLeft = "0";
+    inputInformation.style.marginLeft = "0";
+}
+
+function btnNavShowContacts_onmousedown() {
+    showInformation.style.marginLeft = "-360px";
+    inputInformation.style.marginLeft = "-360px";
+}
